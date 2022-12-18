dd407bc9250f687267ff02cba9e89ca64a7f66a0
agitoreiken <agitoreiken@gmail.com>
2022-12-19 05:06:07 +0900

    fixed binding shader path if plugin is installed in enigne's directory

--- Source/RestyleShaders/Private/RestyleShadersModule.cpp
+++ Source/RestyleShaders/Private/RestyleShadersModule.cpp
@@ -1,12 +1,12 @@
 // Alexander (AgitoReiKen) Moskalenko (C) 2022
 #include "RestyleShadersModule.h"
 #include "ShaderCore.h"
-
+#include <Interfaces/IPluginManager.h>
 #define LOCTEXT_NAMESPACE "FRestyleShadersModule"
 
 void FRestyleShadersModule::StartupModule()
 {
-	FString ShaderDirectory = FPaths::Combine(FPaths::ProjectPluginsDir(), TEXT("/Restyle/Shaders"));
+	FString ShaderDirectory = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("Restyle"))->GetBaseDir(), TEXT("Shaders"));
 	AddShaderSourceDirectoryMapping("/Plugin/Restyle", ShaderDirectory);
 }
 
